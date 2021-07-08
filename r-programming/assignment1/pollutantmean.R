942668e79f27836dea4624d6c62c4a35892c5f4c
cheikhtidi77 <cheikhtidi77@gmail.com>
2021-07-08 22:31:45 +0000

    Pollutant mean

--- r-programming/assignment1/pollutantmean.R
+++ r-programming/assignment1/pollutantmean.R
@@ -1,1 +1,19 @@
+pollutantmean <- function(directory, pollutant, id = 1:332)
+{
+  #create a list of files
+  files_full <- list.files(directory, full.names = TRUE) 
+  # create an empty data frame
+  dat <- data.frame()
+  for (i in id)
+  {
+    #add files to main data
+    dat <- rbind(dat, read.csv(files_full[i]))
+    
+  }
+  #Calulate mean
+  mean_data <- mean(dat[, pollutant], na.rm = TRUE)
+  return(mean_data)
+}
 
+#OUTPUT
+pollutantmean(getwd(), "sulfate", 1:10)
